516a6fb63a981b65fbaf49103d6a6de8d5c3cd91
santmailo <iamsunil006@gmail.com>
2024-09-02 09:52:26 +0530

    attempt 1

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </>
+  );
+}
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-
 import React from "react";
-import './../styles/App.css';
-
+import "./../styles/App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 const App = () => {
   return (
-    <div>
+    <>
+      <div>
         {/* Do not remove the main div */}
-    </div>
-  )
-}
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
